51a3ef28335f4bc99364cf2357ad8387a6cb750e
Evan Kimpton <kimptonevan@gmail.com>
2026-03-23 15:47:29 -0400

    + Logic skeleton

--- ga.py
+++ ga.py
@@ -1,1 +1,14 @@
 # The Genetic Algorithm for creating chromosomes for feature selection
+# Hyperparameters
+CHROMOSOME_LENGTH = 8
+POPULATION_SIZE = 50
+MUTATION_RATE = 0.1
+CROSSOVER_RATE = 0.8
+    
+# Fitness function
+
+# Mutation method
+
+# Crossover Method
+
+# Run generations
