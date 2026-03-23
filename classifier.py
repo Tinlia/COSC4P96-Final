422aed8b61fffe6e452bbf51e78fb8dcc124c02e
Evan Kimpton <kimptonevan@gmail.com>
2026-03-23 19:15:08 -0400

    + RFC Skeleton

--- classifier.py
+++ classifier.py
@@ -1,1 +1,23 @@
 # The Random Forest Classifier for getting the fitness of the GA's chromosomes
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from getdata import get
+
+# Method for train/test using features selected by ga.py
+def evaluate(c: tuple):
+    # Get dataset
+
+    # Select features based on chromosome from GA
+
+    # Split into train:test
+    
+    # Train RFC
+
+    # Predict and calculate accuracy
+    pass
+
+# Calculate the avg accuracy of a chromosome
+def average_accuracy(c: tuple, runs=3):
+    # Run eval thrice
+    pass
+
