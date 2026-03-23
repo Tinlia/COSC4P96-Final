2876964565abf453d6bf8827d0a1e9248b569901
Evan Kimpton <kimptonevan@gmail.com>
2026-03-23 17:48:17 -0400

    + Dataset loading, load(), get()

--- getdata.py
+++ getdata.py
@@ -1,1 +1,12 @@
 # Dataset Loader
+dataset = []
+
+def load():
+    global dataset
+    with open("diabetes.csv", "r") as f:
+        next(f) # Skip header
+        for line in f:
+            dataset.append(line.strip().split(","))
+
+def get() -> list[list]:
+    return dataset
