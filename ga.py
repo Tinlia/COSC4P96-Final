8aebbee0ef64402effa0af16baa1ccbdc368e7dc
Evan Kimpton <kimptonevan@gmail.com>
2026-03-23 19:03:29 -0400

    Optimize fitness eval for 0- and 8-feature chromosomes

--- ga.py
+++ ga.py
@@ -4,16 +4,18 @@ from getdata import load
 
 # Load Dataset
 load()
-
+print("Dataset loaded successfully")
 # Hyperparameters
 CHROMOSOME_LENGTH = 8
-POPULATION_SIZE = 50
+POPULATION_SIZE = 30 
 MUTATION_RATE = 0.2
 CROSSOVER_RATE = 0.7
 ELITISM_RATE = 0.1
-GENERATIONS = 100 
+DIVERSITY_RATE = 0.1
+GENERATIONS = 100000 
 TOURNAMENT_SIZE = 4
-MAX_FITNESS = 1.0 # To be changed to the average accuracy of an 8-feature run of the model
+MAX_FITNESS = 0.9 # To be changed to the average accuracy of an 8-feature run of the model
+COUNT = 0
 
 # Storage
 cache = {} # Caches the average fitness of each chromsome to prevent excessive training (chromosome -> fitness)
@@ -28,15 +30,15 @@ def fitness(c: tuple):
     if cache.get(c) is not None:
         return cache[c]
     
+    # Punish for more features, reward for less features
+    if sum(c) == 8 or sum(c) == 0: # If all or none of the features are selected, punish heavily
+        return 0
+    
     # Else, run it thrice and take the average
     fit = 0
     for _ in range(3):
-        fit += placeholder(c) # Get the accuracy of the model with the features selected by the chromosome
+        fit += placeholder(c) # Get the accuracy of the model with the features selected by the chromosome TODO: Replace with actual model accuracy
     fit /= 3
-
-    # Punish for more features, reward for less features
-    if sum(c) == 8 or sum(c) == 0: # If all or none of the features are selected, punish heavily
-        fit = 0
 
     # Cache fitness and return
     cache[c] = fit
@@ -73,10 +75,6 @@ def gen_chromosome() -> tuple:
             c[i] = 1
     return tuple(c)
 
-# Confirm Hyparams
-if ELITISM_RATE + MUTATION_RATE + CROSSOVER_RATE != 1.0:
-    raise ValueError("Elitism, Mutation, and Crossover rates must sum to 1")
-
 # Gen Random Pop
 pop = []
 for _ in range(POPULATION_SIZE):
@@ -88,11 +86,12 @@ for g in range(GENERATIONS):
     for i in range(POPULATION_SIZE):
         fit = fitness(pop[i][0])
         pop[i] = (pop[i][0], fit) # Update fitness in population
+    
     # Sort fitnesses desc.
     pop.sort(key=lambda x: x[1], reverse=True) 
 
     # Break condition for max fitness
-    if pop[0][1] == MAX_FITNESS:
+    if pop[0][1] >= MAX_FITNESS:
         print(f"Relative accuracy achieved at generation {g} with chromosome {str(pop[0][0])}")
         break
     new_pop = []
@@ -103,7 +102,7 @@ for g in range(GENERATIONS):
         new_pop.append(pop[i])
 
     # Crossover
-    while len(new_pop) < POPULATION_SIZE:
+    while len(new_pop) < POPULATION_SIZE - int(DIVERSITY_RATE * POPULATION_SIZE):
         p1 = tournament_selection(random.sample(pop, TOURNAMENT_SIZE)) # Tournament selection on k random chromosomes
         p2 = tournament_selection(random.sample(pop, TOURNAMENT_SIZE)) # Select 5 random chromosomes and pick the best one as parent 2
         if random.random() < CROSSOVER_RATE:
@@ -113,13 +112,18 @@ for g in range(GENERATIONS):
         new_pop.append((c1, 0))
         new_pop.append((c2, 0))
     
-    if len(new_pop) > POPULATION_SIZE:
-        new_pop.pop() # Remove excess chromosome if we went over population size
+    # Diversity
+    while len(new_pop) < POPULATION_SIZE:
+        new_pop.append((gen_chromosome(), 0))
+
+    while len(new_pop) > POPULATION_SIZE:
+        print("Pop Trimmed")
+        new_pop.pop() # Remove excess chromosomes if we went over population size
 
     # Mutate
-    for i in range(enum, len(new_pop)):
+    for i in range(enum, len(new_pop)-int(DIVERSITY_RATE * POPULATION_SIZE)): # Don't mutate elites or diversity chromosomes
         if random.random() < MUTATION_RATE:
-            new_pop[i] = (mutation(new_pop[i][0]), 0)
-    
+            new_pop[i] = (mutation(new_pop[i][0]), 0.85)
+
     # Update pop
     pop = new_pop
